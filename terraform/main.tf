274dd317eb543f477f3f7b16b5acd3fbec706c2c
Kalamojo <kojo22444488888888@gmail.com>
2026-03-28 18:50:17 -0400

    Testing default path

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 locals {
   site_bucket_id = data.terraform_remote_state.bic_infra.outputs.s3_site_bucket_id
-  site_path      = "${path.root}/../../${var.build_dir}/"
+  site_path      = "${var.build_dir}/"
 }
 
 resource "aws_s3_object" "upload_site" {
-  for_each = fileset(local.site_path, "*")
+  for_each = fileset("${path.root}/", "*")
 
   bucket = local.site_bucket_id
   key    = each.value
