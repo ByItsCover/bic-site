757604178d256b893cacbbc1017ac719a198ad39
Kalamojo <kojo22444488888888@gmail.com>
2026-03-28 21:07:58 -0400

    Forcing rebuild

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>By Its Cover fr</h1>
+          <h1>By Its Cover fr fr</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
