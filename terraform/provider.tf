c52cb93b3da851ff237f5ed39e77284d12b585fd
Kalamojo <kojo22444488888888@gmail.com>
2026-04-25 01:28:39 -0400

    Fixing to only cert removal

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,12 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-/*
+
 provider "aws" {
   alias  = "acm_provider"
   region = "us-east-1"
 }
-*/
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
