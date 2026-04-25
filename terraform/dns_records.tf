1401d4bf7adb92dd423b198c24b07c4057672e07
Kalamojo <kojo22444488888888@gmail.com>
2026-04-25 00:55:10 -0400

    Using correct parameter name

--- terraform/dns_records.tf
+++ terraform/dns_records.tf
@@ -2,7 +2,7 @@ resource "cloudflare_dns_record" "cname" {
   zone_id = data.cloudflare_zone.site_zone.id
   name    = "@"
   type    = "CNAME"
-  value   = aws_cloudfront_distribution.cdn.domain_name
+  content   = aws_cloudfront_distribution.cdn.domain_name
   ttl     = 1
   proxied = true
 }
