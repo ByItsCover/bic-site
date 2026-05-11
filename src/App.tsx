fb35c5626671188c53ff979f781795864488aca3
Kalamojo <kojo22444488888888@gmail.com>
2026-05-10 22:28:37 -0400

    Updating grid and removing unused page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import {Route, Routes } from "react-router-dom"
-import Home from "./pages/home/Home.tsx";
 import Search from "./pages/search/Search.tsx";
 
 function App() {
   return (
       <Routes>
           <Route index element={<Search />} />
-          <Route path="home" element={<Home />} />
       </Routes>
   )
 }
