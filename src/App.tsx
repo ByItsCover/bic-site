a310b3b5dff39dabe49e0fe7aeed785245f21f71
Kalamojo <kojo22444488888888@gmail.com>
2026-06-15 00:09:51 -0400

    Added recommendation page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Route, Routes } from "react-router-dom"
 import Search from "./pages/search/Search.tsx";
+import Recommend from "./pages/recommend/Recommend.tsx";
 
 function App() {
   return (
       <Routes>
           <Route index element={<Search />} />
+          <Route path="recommend" element={<Recommend />} />
       </Routes>
   )
 }
---
+++ src/pages/recommend/Recommend.tsx
@@ -0,0 +1,10 @@
+const Recommend = () => {
+    return (
+        <>
+            <h1>Recommendation Page</h1>
+            <p>Under Construction</p>
+        </>
+    )
+}
+
+export default Recommend;
