5748e9fe086972e4e075a37d431c8d8b467ff0af
Kalamojo <kojo22444488888888@gmail.com>
2026-07-01 02:51:55 -0400

    Adding multi-line inputs

--- src/utils/formHelper.ts
+++ src/utils/formHelper.ts
@@ -1,7 +1,18 @@
-export const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement, HTMLInputElement>, 
+const handleInputChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement, HTMLTextAreaElement>,
     setState: React.Dispatch<React.SetStateAction<string>>
 ) => {
 
     setState(event.target.value);
 };
+
+const handleEnterPress = (
+    event: React.KeyboardEvent<HTMLTextAreaElement>,
+    searchSubmit: React.EventHandler<React.SubmitEvent<HTMLFormElement> | React.KeyboardEvent<HTMLTextAreaElement>>
+) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        searchSubmit(event);
+    }
+};
+
+export { handleInputChange, handleEnterPress };
