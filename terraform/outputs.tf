ac81e4d61738d2d6a671668555b96c5394541720
Kalamojo <kojo22444488888888@gmail.com>
2026-08-09 14:14:15 -0400

    Adding cognito back

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,7 @@ output "website_url" {
   value       = "https://${var.domain_name}"
 }
 
-# output "cognito_pool_client_id" {
-#   description = "Client ID for Cognito User Pool"
-#   value       = aws_cognito_user_pool_client.auth_client.id
-# }
+output "cognito_pool_client_id" {
+  description = "Client ID for Cognito User Pool"
+  value       = aws_cognito_user_pool_client.auth_client.id
+}
